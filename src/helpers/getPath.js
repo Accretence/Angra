46e6c634c877cc06bc06a12395711c00521b7da1
Accretence <Accretence@users.noreply.github.com>
2022-08-07 01:03:27 +0430

    Debugging

--- src/helpers/getPath.js
+++
@@ -1,6 +0,0 @@
-import path from 'path'
-import process from 'process'
-
-export default async function (name) {
-	return path.join(process.cwd(), 'node_modules', 'angra', 'views', name)
-}
---
+++ src/markdown/getEpilogue.js
@@ -0,0 +1,42 @@
+export default function () {
+	return `<!-- footer -->
+					<table
+						class="footer-wrap"
+						cellpadding="0"
+						cellspacing="0"
+						border="0"
+						align="center"
+					>
+						<tr>
+							<td></td>
+							<td class="container">
+								<!-- content -->
+								<div class="content">
+									<table>
+										<tr>
+											<td align="center" valign="top">
+												<p>
+													Don't like these emails?
+													<a href="#"
+														><unsubscribe
+															>Unsubscribe</unsubscribe
+														></a
+													>.
+												</p>
+											</td>
+										</tr>
+									</table>
+								</div>
+								<!-- /content -->
+							</td>
+							<td></td>
+						</tr>
+					</table>
+					<!-- /footer -->
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
+`
+}
