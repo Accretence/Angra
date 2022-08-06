87a96d53f483c5239774b4d0beb84eb4e1ea6732
Accretence <Accretence@users.noreply.github.com>
2022-08-06 16:46:42 +0430

    Debugging

--- src/helpers/getPath.js
+++ src/helpers/getPath.js
@@ -2,5 +2,5 @@ import path from 'path'
 import process from 'process'
 
 export default async function (name) {
-	return path.join(process.cwd(), 'views', name)
+	return path.join(process.cwd(), 'node_modules', 'angra', 'views', name)
 }
