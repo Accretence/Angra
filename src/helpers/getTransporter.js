7d6041d81dcfa42d8d7b974e08f2c7e6d00bc03d
Accretence <Accretence@users.noreply.github.com>
2022-08-08 10:52:39 +0430

    SMTP Connection method as env variable

--- src/helpers/getTransporter.js
+++ src/helpers/getTransporter.js
@@ -4,12 +4,13 @@ const host = process.env.MAIL_SMTP_HOST
 const port = process.env.MAIL_SMTP_PORT
 const user = process.env.MAIL_SMTP_USER
 const pass = process.env.MAIL_SMTP_PASS
+const secure = process.env.MAIL_SMTP_SECURE
 
 export default async function () {
 	return nodemailer.createTransport({
 		host,
 		port,
-		secure: true,
+		secure: secure == 'true' ? true : false,
 		auth: {
 			user,
 			pass,
