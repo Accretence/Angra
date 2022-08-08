4f2809ba957bc2651dc7942d1421dcd2890bf7ed
Accretence <Accretence@users.noreply.github.com>
2022-08-08 12:48:02 +0430

    Advanced Connection Options

--- src/helpers/getTransporter.js
+++ src/helpers/getTransporter.js
@@ -5,6 +5,9 @@ const port = process.env.MAIL_SMTP_PORT
 const user = process.env.MAIL_SMTP_USER
 const pass = process.env.MAIL_SMTP_PASS
 const secure = process.env.MAIL_SMTP_SECURE
+const tls = process.env.MAIL_SMTP_TLS_REJECT_UNAUTHORIZED
+const cipher = process.env.MAIL_SMTP_TLS_CIPHER
+const service = process.env.MAIL_SMTP_SERVICE
 
 export default async function () {
 	return nodemailer.createTransport({
@@ -15,5 +18,10 @@ export default async function () {
 			user,
 			pass,
 		},
+		tls: {
+			rejectUnauthorized: tls == 'true' ? true : false,
+			ciphers: cipher ? cipher : null,
+		},
+		service: service ? service : null,
 	})
 }
